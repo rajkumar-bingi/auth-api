bd21ac52508191331b430fc5d2019f16b7c1940b
rajkumar-bingi <rajkumarbingi06@gmail.com>
2022-08-19 01:43:23 +0530

    profile and change and reset mail end points created

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from account.views import UserLoginView, UserRegistrationView
+from account.views import SendPasswordResetEmailView, UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
+    path('send-reset-password-email/', SendPasswordResetEmailView.as_view(),
+         name='send-reset-password-email'),
 ]
