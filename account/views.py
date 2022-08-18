69e3bea54b4c90527b540d36fa732a07a1a88420
rajkumar-bingi <rajkumarbingi06@gmail.com>
2022-08-19 00:01:18 +0530

    register and login views created

--- account/views.py
+++ account/views.py
@@ -1,9 +1,35 @@
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import status
+from account.models import User
+from django.contrib.auth import authenticate
+
+from account.serializers import UserLoginSerializer, UserRegistrationSerializer
+from account.renderers import UserRenderer
 
 
 # Create your views here.
 class UserRegistrationView(APIView):
-    def get(self, requet, format=None):
-        return Response({"msg": 'Registration succesful!'})
+    renderer_classes = [UserRenderer]
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({"msg": 'Registration succesful!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    renderer_classes = [UserRenderer]
+
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get("email")
+            password = serializer.data.get("password")
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                return Response({'msg': 'Login Success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': {'non_field_errors': ['Email or Password is not valid']}}, status=status.HTTP_404_NOT_FOUND)
